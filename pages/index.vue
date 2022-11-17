8d560b42aba58abe7f5885a841a326cd891e4406
shreydd <shreyasramesh08@gmail.com>
2022-11-17 17:18:12 +0530

    chore: UI and logic clean ups

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,83 @@
 <template>
-  <section
-    class="
-      container
-      max-w-lg
-      mx-auto
-      px-5
-      flex flex-col
-      justify-center
-      h-screen
-    "
-  >
-    <img
-      src="../assets/images/wc-logo.svg"
-      alt="WisdomCircle logo"
-      class="max-h-12 mb-10 xl:hidden"
-    />
-    <h1 class="">Sign in to WisdomCircle</h1>
-    <p class="">
-      Don't have an account?
-      <span class="text-royal-blue-4 font-bold">Sign up</span>
-    </p>
+  <section class="grid xl:grid-cols-12">
+    <section class="hidden xl:flex col-span-5 h-screen bg-neutral-grey flex-col">
 
-    <VForm @submit="submitHandler" class="flex flex-col gap-4 mt-12">
-      <VField
-        name="phoneOrEmail"
-        :rules="PhoneOrEmailRules"
-        class=""
-        placeholder="Phone number or Email"
+        <div class="h-1/2 flex justify-center">
+            <img src="../assets/images/wc-logo-desktop.svg" alt="wisdom circle logo" class="max-h-40 mt-28">
+        </div>
+        
+        <section class="flex flex-1">
+
+            <img src="../assets/images/mascot_pose.svg" class="max-h-38 flex self-end" alt="posing mascot">
+            
+            <div class="flex flex-col justify-between">
+                <div class="">
+                    <h1 class="text-white mb-3">Welcome back!</h1>
+                    <p class="text-white">Sign In to find opportunities that match your interests. We have both part-time and full-time roles that can be done online and in-person.</p>
+                </div>
+                
+                    <img src="../assets/images/dotted_lines.svg" class="basis-0 self-start" alt="dotted lines">
+                    <small class="mb-6 text-white">Please contact us at <b>+91-9380644532</b> if you need any assistance.</small>
+            </div>
+        </section>
+
+    </section>
+
+    <section
+      class="
+        container
+        max-w-lg
+        mx-auto
+        px-5
+        flex flex-col
+        justify-center
+        h-screen
+        col-span-7
+      "
+    >
+      <img
+        src="../assets/images/wc-logo.svg"
+        alt="WisdomCircle logo"
+        class="max-h-12 mb-10 xl:hidden"
       />
-      <VErrorMessage
-        name="phoneOrEmail"
-        as="span"
-        class="text-red-600"
-      ></VErrorMessage>
+      <h1 class="">Sign in to WisdomCircle</h1>
+      <p class="">
+        Don't have an account?
+        <span class="text-royal-blue-4 font-bold">Sign up</span>
+      </p>
 
-      <VField
-        name="password"
-        type="password"
-        rules="passwordRules"
-        class=""
-        placeholder="Password"
-      />
-      <VErrorMessage name="password" class="text-red-600"></VErrorMessage>
+      <VForm @submit="submitHandler" class="flex flex-col gap-4 mt-12">
+        <VField
+          name="phoneOrEmail"
+          :rules="PhoneOrEmailRules"
+          class=""
+          placeholder="Phone number or Email"
+        />
+        <VErrorMessage
+          name="phoneOrEmail"
+          as="span"
+          class="text-red-600"
+        ></VErrorMessage>
 
-      <p class="text-royal-blue-4 font-bold text-right">Forgot Password</p>
+        <VField
+          name="password"
+          type="password"
+          rules="passwordRules"
+          class=""
+          placeholder="Password"
+        />
+        <VErrorMessage name="password" class="text-red-600"></VErrorMessage>
 
-      <button class="primary-btn mt-20 lg:mt-0">Sign in</button>
-    </VForm>
+        <p class="text-royal-blue-4 font-bold text-right">Forgot Password</p>
+
+        <button class="primary-btn mt-20 font-bold lg:mt-0">Sign in</button>
+      </VForm>
+    </section>
   </section>
 </template>
 
 <script setup>
 import { email, numeric } from "@vee-validate/rules";
-import { useResetForm } from "vee-validate";
 
 let validUsers = [];
 let userExists = false;
@@ -102,17 +127,11 @@ const submitHandler = async (formValues, actions) => {
     },
   });
 
-  const isValidUser = validateUser(formValues, actions);
-
-  if (isValidUser) {
-    console.log("herehere");
-    const resetForm = useResetForm();
-    resetForm(); // resets the for
-  }
+  validateUser(formValues, actions);
+  //   TODO: reset form after submission or move to next page?
 };
 
 const validateUser = (formValues, actions) => {
-
   userExists = false;
   // check if email and user email ID exists
   if (email(formValues.phoneOrEmail)) {
@@ -138,7 +157,6 @@ const validateUser = (formValues, actions) => {
 
   // check if ph no and user ph no exists
   else if (numeric(formValues.phoneOrEmail)) {
-
     for (const validUser of validUsers) {
       if (validUser.phNo === formValues.phoneOrEmail) {
         if (validUser.password === formValues.password) {
